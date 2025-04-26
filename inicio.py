86cb57260be98c0bcd89331d395d2a742d8f237d
GERRYP20 <gerryperezsanchez2@gmail.com>
2025-04-26 00:54:43 -0600

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -1,10 +1,10 @@
 from mnu import mostrar_menu
 from inter import seleccion_de_personaje
-from main import iniciar_juego  # Asegúrate de importar esto
+from main import iniciar_juego 
 from Acciones.sonidos import *
 
 if __name__ == "__main__":
-    sonidoOn("sonidos/SoteMenu.mp3")  # Primero arranca la música
+    sonidoOn("sonidos/SoteMenu.mp3") 
     while True:
         accion = mostrar_menu("Imagenes/SoteImage.png")
         if accion == "jugar":
